48dec2ec61f6e8529691b73cc3aed476024af1b9
jh0100c <jh0100c@gmail.com>
2026-03-27 09:19:57 +0900

    upload 1

---
+++ code/1-1/1-1-foo.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+int foo(int a, int b) {
+    return a+b;
+}
---
+++ code/1-3/util.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+static inline void logger_static_inline(char* message) {
+    static int counter = 0;
+    printf("log %2d: %s\n", counter++, message);
+}
